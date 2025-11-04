5448f288fb461c234e00f48b0a85b9421858242d
Pavel Smalenski <smalenski.p.a@gmail.com>
2025-11-04 18:27:44 +0300

    Add initial test

---
+++ FinDataApi.Tests/WeeklyBalancesTests.cs
@@ -0,0 +1,15 @@
+﻿using Moq;
+using Reports.Rpt1WeeklyBalance.Entities;
+
+namespace FinDataApi.Tests;
+
+public class WeeklyBalancesTests
+{
+    [Fact]
+    public void Totals_ReturnsSumm()
+    {
+        WeeklyBalances weeklyBalances = new WeeklyBalances(1, 2, 3, 4, 5, 6);
+
+        Assert.True(weeklyBalances.Total == 21);
+    }
+}
